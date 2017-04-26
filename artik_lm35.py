5013909a807b335dee3f620546fa05e29bd375bb
Tomasz Szydlo <tszydlo@agh.edu.pl>
2017-04-27 00:23:20 +0200

    First commit

---
+++ artik_lm35.py
@@ -0,0 +1,36 @@
+import requests
+import pprint
+import time
+import sys
+from upm import pyupm_lm35 as sensorObj
+
+artikcloud = "https://api.artik.cloud/v1.1/messages"
+bearer = ''
+sdid = ''
+
+if len(sys.argv) == 3:
+    bearer = 'Bearer ' + sys.argv[1]
+    sdid = sys.argv[2]
+
+    print(bearer +" "+ sdid)
+
+else:
+    print("Usage: temp_client.py [token] [device_id]")
+    sys.exit(0)
+
+headers_dict =  {
+     "Content-Type": "application/json",
+     "Authorization": bearer
+   }
+
+sensor = sensorObj.LM35(0)
+
+while True:
+    temp = sensor.getTemperature()
+    print("Temperature:", temp , "C")
+    data_dict = ' {"sdid": "' + sdid + '","type": "message","data": {"temperature": "'+str(temp)+'" } }'
+    r = requests.post(artikcloud, headers = headers_dict, data=data_dict)
+    #pprint.pprint(r.headers)
+    pprint.pprint(r.content)
+    time.sleep(10)
+
