aed355d7037c22d91249ad18f6478e3f1f96b9e0
Tomasz Szydlo <tszydlo@agh.edu.pl>
2017-04-27 01:26:36 +0200

    Added gpio testing.

--- artik_lm35.py
+++ artik_lm35.py
@@ -3,6 +3,7 @@ import pprint
 import time
 import sys
 from upm import pyupm_lm35 as sensorObj
+import mraa
 
 artikcloud = "https://api.artik.cloud/v1.1/messages"
 bearer = ''
@@ -25,7 +26,11 @@ headers_dict =  {
 
 sensor = sensorObj.LM35(0)
 
+led1 = mraa.Gpio(0)
+led1.dir(mraa.DIR_OUT)
+
 while True:
+    led1.write(1)
     temp = int(sensor.getTemperature())
     print("Temperature:", temp , "C")
     data_dict = ' {"sdid": "' + sdid + '","type": "message","data": {"temperature": "'+str(temp)+'" } }'
